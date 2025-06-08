0954445a4f0635b5f6bfb041fc59a7e68bf621ab
nehmiya <144734399+nehmiya@users.noreply.github.com>
2025-06-08 11:31:19 +0300

    Finished Building second section

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer.jsx';
 import Alert from './Components/Alert/Alert.jsx';
 import FirstSection from './Components/Section/FirstSection.jsx';
+import SecondSection from './Components/Section/SecondSection.jsx';
 import "./App.css";
-
 
 function App() {
 
@@ -12,12 +12,10 @@ function App() {
       <Header />
       <Alert />
       <FirstSection />
-
+      <SecondSection/>
       <Footer />
-
-      
     </>
-  )
+  );
 }
 
 export default App
